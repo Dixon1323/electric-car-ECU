52808ff38d6ece92345da02bf7ab417ba12fc366
Dixon1323 <dixonsunny1323@gmail.com>
2023-03-31 13:12:41 +0530

    parsed all the data

---
+++ serial_data_read/serial_data_read.ino
@@ -0,0 +1,42 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup() {
+  // Start serial communication
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  // Read data from the module
+  if (mySerial.available() > 0) {
+    String data = mySerial.readStringUntil('\n');
+    Serial.println(data);
+    
+    // Extract voltage, energy, and charge values from the data
+    float current = getValue(data, 'C');
+    float voltage = getValue(data, 'V');
+    float energy = getValue(data, 'W');
+    float charge = getValue(data, 'A');
+    
+    // Print values to serial monitor
+    Serial.print("Current:");
+    Serial.print(current);
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+    Serial.print(" V, Energy: ");
+    Serial.print(energy);
+    Serial.print(" Wh, Charge: ");
+    Serial.print(charge);
+    Serial.println(" Ah");
+  }
+}
+
+float getValue(String data, char code) {
+  int index = data.indexOf(code) + 1; // Skip the code character
+  int endIndex = data.indexOf('D', index); // Find the end delimiter
+  String valueStr = data.substring(index, endIndex);
+  float value = valueStr.toFloat();
+  return value;
+}
